34152ffb76f1308cb10cbc21754d59f62c417125
Soroush <ranjbar.soroush32@gmail.com>
2023-11-11 09:57:12 +0330

    Title component is created and added to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Image } from "./components"
+import { Image, Title } from "./components"
 
 function App() {
 
   return (
     <div className={`flex min-h-screen justify-center items-center`}>
-      <div className={'flex w-4/12 border-red-100'}>
+      <div className={'flex flex-col w-4/12 border-red-100'}>
         <Image />
+        <Title>Lizard</Title>
       </div>
     </div>
   )
---
+++ src/components/Title.jsx
@@ -0,0 +1,6 @@
+
+export const Title = ({ children }) => {
+  return (
+    <h1 className="p-4 text-2xl">{children}</h1>
+  )
+}
